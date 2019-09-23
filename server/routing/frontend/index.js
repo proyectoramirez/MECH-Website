1b32de9352a8bed2b002d96151b69fdf77054934
Jose Ramirez <proyectoramirez07@gmail.com>
2019-09-22 23:04:15 -0400

    Setups for different environments are only required in that environment

--- server/routing/frontend/index.js
+++ server/routing/frontend/index.js
@@ -1,7 +1,4 @@
 const { isDev } = require("../../utils/env");
-const devSetup = require("./devSetup");
-const prodSetup = require("./prodSetup");
-
-const router = isDev ? devSetup() : prodSetup();
+const router = isDev ? require("./devSetup")() : require("./prodSetup")();
 
 module.exports = router;
