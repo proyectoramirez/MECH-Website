c52ee57f72dfb17cb2c7ca368c7333af7f7d798f
Jose Ramirez <proyectoramirez07@gmail.com>
2019-09-22 21:45:10 -0400

    Adjusted routes before deployment

--- server/routing/index.js
+++ server/routing/index.js
@@ -2,10 +2,8 @@ const express = require("express");
 const config = require("@/config");
 const loadPathsIntoRouter = require("@/utils/loadPathsIntoRouter");
 const frontEnd = require("./frontend");
-const api = require("./api");
 
 const routes = [
-    ["/api", api],
     [config.frontend_public_path, frontEnd],
     ["/", express.static("public")]
 ];
